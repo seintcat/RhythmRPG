0dd1535385ce0cb58dd1cd0c30b3c47cf17ce76b
seintcat <1x2x0x7@naver.com>
2023-12-04 17:53:36 +0900

    스테이지 선택화면 제작중

---
+++ RhythmRPG/Assets/_Scripts/StageButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StageButton : MonoBehaviour
+{
+    [SerializeField]
+    private List<RectTransform> clockWiseStartUp;
+    [SerializeField]
+    TextMeshProUGUI stageName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
